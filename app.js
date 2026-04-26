7c73ec3b0863f41c02789ff227f706cbada0d637
dida0982 <dida0982@gmail.com>
2026-04-26 14:20:56 -0300

    commented tests with let, alert, prompt, is, else
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -8,10 +8,27 @@ if ( numberSecret == kick ) {
 }
 */
 
-alert( 'Welcome to our website.' )
-let nome = 'Moon';
-let age = 25;
-let numberOfSales = 50;
-let availableBalance = 1000;
-alert( 'Error! Please fill in all fields.' );
 
+//Test:
+//alert( 'Welcome to our website.' )
+
+//let name = 'Moon';
+//let age = 25;
+//let numberOfSales = 50;
+//let availableBalance = 1000;
+
+//alert( 'Error! Please fill in all fields.' );
+
+//let mensageOfError = 'Error! Please fill in all fields.';
+//alert( mensageOfError );
+
+//let name = prompt( 'What is your name?' );
+//console.log(name)
+
+//let age = prompt('How old are your?')
+//console.log( age )
+
+//if ( age >= 18 ){
+//    alert( 'Can get a drivers license.' );
+//}
+//else { alert( 'You cant get your drivers license.' ); }
