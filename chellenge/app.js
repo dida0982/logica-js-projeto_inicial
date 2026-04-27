39e05dbb1937be449419f1b2cfcc1ab333447938
dida0982 <dida0982@gmail.com>
2026-04-27 20:41:55 -0300

    add tests
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- chellenge/app.js
+++ chellenge/app.js
@@ -12,10 +12,10 @@ else
 {
     alert( 'Have a good week' );
 }
-*/
+
 
 //test2
-/*
+
 let number = prompt( 'Enter a number? ' );
 console.log( 'Number: ', number );
 
@@ -24,10 +24,10 @@ if ( number % 2 === 0 );
 alert( 'Number even' );
 }
 else { alert('number odd') };
-*/
+
 
 //test3
-/*
+
 let number = prompt( 'Enter a number.' );
 
 if (number >= 100) {
@@ -35,16 +35,112 @@ if (number >= 100) {
 } else {
     alert('Try again to win.');
 }
-*/
+
 
 //test4
-/*
+
 let name = prompt( 'Enter your name.' );
 alert(`Welcome ${name}`);
-\ */
+
+
 
 //test5
 let accountBalance = 500;
 alert( `Your account balance is R$${ accountBalance }` );
 console.log( `account balance: R$`, accountBalance );
 
+
+//test6
+
+let qtdNumeros = prompt( 'Digite a quantidade de números para o cálculo da média:' );
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+}
+
+let media = soma / qtdNumeros;
+contador--
+
+console.log(media);
+
+
+//test7
+
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+let numero = parseInt( prompt( 'Digite o numero:' ) );
+soma += numero;
+contador--;
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
+
+//test8
+//Create a counter that starts at 1 and goes up to 10 using a while loop. Display each number.
+
+let number = 1
+
+while (number <= 10)
+{
+console.log( number )
+number++
+}
+
+//test9
+//Create a counter that start at 10 and goes up to 0 using a while loop. Diplay each number.
+
+let number = 10;
+
+while (number >= 0) {
+    console.log(number)
+    number--
+}
+
+//test10
+//Create a programmer the counter regressive. Have a number and convert to number and 0, use in a while loop to console the navigator.
+
+let number = 0;
+let chooseANumber = prompt( `Choose a number` );
+
+console.log( `Your choose the number: ${ chooseANumber } ` );
+
+while (chooseANumber >= number) {
+    console.log( `From number ${ chooseANumber } to ${ number }` );
+    chooseANumber--;
+}
+
+test11
+//Create a program for counting forward. Ask for a number and count from 0 to that number using a while loop in the browser console.
+let number = 0;
+let chooseNumber = prompt( `Choose a number.` );
+
+console.log( `Choose a number: ${ chooseNumber }` );
+
+while ( number <= chooseNumber )
+{
+console.log( `Number: ${ number }.` );
+number++;
+}
+
+test12
+let idade = 25;
+let possuiCarteira = true;
+
+// se idade é maior que 18 e possui carteira…
+if (idade > 18 && possuiCarteira) {
+  console.log("Pode dirigir!");
+} else {
+  console.log("Não pode dirigir.");
+}
+
+
+
+*/
--- chellenge/test/test2.js
+++
@@ -1,6 +0,0 @@
-let contador = 1;
-
-while (contador < 4) {
-  console.log('Executando a interação ' + contador);
-  contador = contador + 1;
-}
