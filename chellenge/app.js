659923d3517217d60b0a5c009e61f4f4e8d7ed72
dida0982 <dida0982@gmail.com>
2026-04-27 00:48:05 +0000

    26 de abr. de 2026, 21:48

--- chellenge/app.js
+++ chellenge/app.js
@@ -4,7 +4,7 @@
 let dayOfWeek = prompt( 'What day of the week is it?' );
 console.log( 'chosen day: ', dayOfWeek );
 
-if ( dayOfWeek == 'saturday' || dayOfWeek == 'sunday' )
+if ( dayOfWeek == 'saturday' || dayOfWeek == 'sunday' );
 {
     alert( 'Have a good weekend.' );
 }
@@ -17,9 +17,9 @@ else
 //test2
 /*
 let number = prompt( 'Enter a number? ' );
-console.log( 'Number: ', number )
+console.log( 'Number: ', number );
 
-if ( number % 2 === 0 )
+if ( number % 2 === 0 );
 {
 alert( 'Number even' );
 }
@@ -28,22 +28,23 @@ else { alert('number odd') };
 
 //test3
 /*
-let number = prompt( 'Enter a number.' )
+let number = prompt( 'Enter a number.' );
 
 if (number >= 100) {
-    alert('Congratulation, you win.')
+    alert('Congratulation, you win.');
 } else {
-    alert('Try again to win.')
+    alert('Try again to win.');
 }
 */
 
 //test4
 /*
-let name = prompt( 'Enter your name.' )
-alert(`Welcome ${name}`)
+let name = prompt( 'Enter your name.' );
+alert(`Welcome ${name}`);
 \ */
 
 //test5
 let accountBalance = 500;
-alert(`Your account balance is R$${accountBalance}`)
-console.log(`account balance: R$`,accountBalance)
+alert( `Your account balance is R$${ accountBalance }` );
+console.log( `account balance: R$`, accountBalance );
+
