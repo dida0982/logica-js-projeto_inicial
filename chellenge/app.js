5eb95a7f12a908a3545313fb28e92c12c820d34e
dida0982 <dida0982@gmail.com>
2026-04-29 12:21:34 -0300

    9807
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- chellenge/app.js
+++ chellenge/app.js
@@ -186,8 +186,9 @@ console.log(programmingLanguage);
 
 //Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
 
-*/
 let value1 = Number(prompt( `Escolha um valor ` ));
 let value2 = Number(prompt( `Escolha outro valor` ));
 let result = value1 + value2;
 console.log(`A soma de ${value1} e ${value2} é igual a ${result}.`);
+
+*/
